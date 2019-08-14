d59ab0b3304505448bf137bab4246376abc6522d
Marcin <marcin96semkowski@gmail.com>
2019-08-14 17:50:45 +0200

    Singleton : GameEngine

---
+++ src/Singleton/Game/GameEngine.java
@@ -0,0 +1,17 @@
+package Singleton.Game;
+
+public class GameEngine {
+
+    private  int hp = 100;
+
+    private String characterName = " ";
+public  void run(){
+    while (true){
+        //czekamy na input gracza
+        //zmieniamy stan gry
+        // renderujemy grafikę
+    }
+
+}
+
+}

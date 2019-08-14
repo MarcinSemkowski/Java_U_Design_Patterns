1e79366f14af790e1a945bb88f47f24e44ac2105
Marcin <marcin96semkowski@gmail.com>
2019-08-14 18:24:50 +0200

    Singleton : multithreading problem solution #1

--- src/Singleton/Game/Main.java
+++ src/Singleton/Game/Main.java
@@ -2,6 +2,13 @@ package Singleton.Game;
 
 public class Main {
 
+    public static void main(String[] args) {
+        GameEngine gameEngine1 = GameEngine.getInstance();
 
+        GameEngine gameEngine2 = GameEngine.getInstance();
+
+        System.out.println(gameEngine1==gameEngine2);
+
+    }
 
 }

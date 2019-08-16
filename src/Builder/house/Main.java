03475957b3514e74b8b42701eb2340e4df84234b
Marcin <marcin96semkowski@gmail.com>
2019-08-16 21:19:16 +0200

    Builder: Main

--- src/Builder/house/Main.java
+++ src/Builder/house/Main.java
@@ -6,6 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        House house = new  House.HouseBuilder()
+                .buildWalls("walls")
+                .buildFloors("floors")
+                .buildRoof("roof")
+                .buildRooms("rooms")
+                .build();
+        System.out.println(house);
     }
 
 }

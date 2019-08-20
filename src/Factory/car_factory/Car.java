e1c96e36e5f3ba3ce7f7ceea4b340360a62da2c9
Marcin <marcin96semkowski@gmail.com>
2019-08-20 17:53:55 +0200

    Factory: abstract Factory : car_factory: #2 Cars

--- src/Factory/car_factory/Car.java
+++ src/Factory/car_factory/Car.java
@@ -2,13 +2,15 @@ package Factory.car_factory;
 
 public abstract class Car {
 
-private String engineCapacity;
+    private String engineCapacity;
 
-private  FuelType fuelType;
+    private  FuelType fuelType;
 
-private int yearOfProduction;
+    private int yearOfProduction;
 
-
-
-
+    public Car(String engineCapacity, FuelType fuelType, int yearOfProduction) {
+        this.engineCapacity = engineCapacity;
+        this.fuelType = fuelType;
+        this.yearOfProduction = yearOfProduction;
+    }
 }

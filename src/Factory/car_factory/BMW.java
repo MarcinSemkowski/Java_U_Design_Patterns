4029c051eeb954bc1eb2cf8422d4cace810178b5
Marcin <marcin96semkowski@gmail.com>
2019-08-20 17:54:07 +0200

    Factory: abstract Factory : car_factory: #2 Cars

---
+++ src/Factory/car_factory/BMW.java
@@ -0,0 +1,7 @@
+package Factory.car_factory;
+
+public class BMW extends Car {
+    public BMW(String engineCapacity, FuelType fuelType, int yearOfProduction) {
+        super(engineCapacity, fuelType, yearOfProduction);
+    }
+}
---
+++ src/Factory/car_factory/Ford.java
@@ -0,0 +1,7 @@
+package Factory.car_factory;
+
+public class Ford extends Car {
+    public Ford(String engineCapacity, FuelType fuelType, int yearOfProduction) {
+        super(engineCapacity, fuelType, yearOfProduction);
+    }
+}

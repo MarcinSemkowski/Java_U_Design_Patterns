bb8a3bdfbfc6c1688d83686c49c0041935000b65
Marcin <marcin96semkowski@gmail.com>
2019-08-21 18:09:26 +0200

    Flyweight #5

--- src/Flyweight/UnitStatsRepository.java
+++ src/Flyweight/UnitStatsRepository.java
@@ -2,6 +2,41 @@ package Flyweight;
 
 public class UnitStatsRepository {
 
+    private static UnitStats unitStats = new UnitStats("Destroyer"
+            ,500
+            ,250
+            ,70
+            ,10
+            ,800);
 
 
+    private static UnitStats teslaTankUnitStats = new UnitStats("TeslaTank"
+            ,200
+            ,100
+            ,50
+            ,25
+            ,500);
+
+    private static UnitStats riflemanUnitStats = new UnitStats("Rifleman",25,5,20,25,50);
+
+
+
+
+
+
+    private UnitStatsRepository(){}
+
+
+    public static UnitStats getDestroyerStats(){
+        return unitStats;
+    }
+
+
+    public static UnitStats getTeslaTankUnitStats() {
+        return teslaTankUnitStats;
+    }
+
+    public static UnitStats getRiflemanUnitStats() {
+        return riflemanUnitStats;
+    }
 }

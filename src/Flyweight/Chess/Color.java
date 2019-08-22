6b6450e5a32e1b14b28ca85c185d504a7125cb1d
Marcin <marcin96semkowski@gmail.com>
2019-08-22 16:01:53 +0200

    Flyweight Chess #1

---
+++ src/Flyweight/Chess/Color.java
@@ -0,0 +1,15 @@
+package Flyweight.Chess;
+
+public class Color {
+
+private int r;
+private int g;
+private int b;
+
+
+    public Color(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
---
+++ src/Flyweight/Chess/ColorRepository.java
@@ -0,0 +1,16 @@
+package Flyweight.Chess;
+
+public class ColorRepository {
+
+private static Color white = new Color(0,0,0);
+private static Color black = new Color(255,255,255);
+
+
+    public static Color getWhite() {
+        return white;
+    }
+
+    public static Color getBlack() {
+        return black;
+    }
+}

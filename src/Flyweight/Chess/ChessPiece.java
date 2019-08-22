a81524522a7c06082c514ef40ac68ff48b0930e2
Marcin <marcin96semkowski@gmail.com>
2019-08-22 16:08:12 +0200

    Flyweight Chess #2

---
+++ src/Flyweight/Chess/ChessPiece.java
@@ -0,0 +1,22 @@
+package Flyweight.Chess;
+
+public abstract class ChessPiece {
+
+String name;
+String numberPosition;
+String letterPosition;
+Color color;
+
+
+    public ChessPiece(String name, String numberPosition, String letterPosition, String color) {
+        this.name = name;
+        this.numberPosition = numberPosition;
+        this.letterPosition = letterPosition;
+        switch (color) {
+            case "black":
+                this.color = ColorRepository.getBlack();
+                 default:
+                     this.color = ColorRepository.getWhite();
+        }
+        }
+}

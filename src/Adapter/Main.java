ae15985707dfa2d2ff324cf553e9c108ab02f6e5
Marcin <marcin96semkowski@gmail.com>
2019-08-23 16:22:14 +0200

    Adapter Intro

---
+++ src/Adapter/Main.java
@@ -0,0 +1,39 @@
+package Adapter;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       /* ContinentalDevice radio = new ContinentalDevice() {
+            @Override
+            public void on() {
+                System.out.println("Gra muzyka");
+            }
+        } ;
+        */
+
+
+       ContinentalDevice radio = ()-> System.out.println("Gra muzyka");
+
+      ContinentalSocket continentalSocket = new ContinentalSocket();
+      continentalSocket.plugIn(radio);
+
+      /*
+      UKDevice ukRadio = new UKDevice() {
+          @Override
+          public void powerOn() {
+              System.out.println("Gra muzyka z UK");
+          }
+      };
+
+       */
+
+
+      UKDevice ukRadio = ()-> System.out.println("Gra muzyka z UK");
+
+      UKSocket ukSocket = new UKSocket();
+      ukSocket.plugIn(ukRadio);
+
+
+    }
+}

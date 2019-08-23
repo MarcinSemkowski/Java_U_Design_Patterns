c175982a5916e89aee88ab1887d4f29acd2383ed
Marcin <marcin96semkowski@gmail.com>
2019-08-23 16:13:08 +0200

    Adapter #2

---
+++ src/Adapter/ContinentalSocket.java
@@ -0,0 +1,10 @@
+package Adapter;
+
+public class ContinentalSocket {
+
+public void plugIn(ContinentalDevice device){
+    device.on();
+}
+
+
+}
---
+++ src/Adapter/UKSocket.java
@@ -0,0 +1,9 @@
+package Adapter;
+
+public class UKSocket {
+
+ public  void plugIn(UKDevice device){
+     device.powerOn();
+ }
+
+}

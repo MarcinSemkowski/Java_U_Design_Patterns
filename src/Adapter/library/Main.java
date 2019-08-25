f9b62b959501df5ba71da790913d9e7c6b875572
Marcin <marcin96semkowski@gmail.com>
2019-08-25 17:48:07 +0200

    Adapter Library #4 Main

---
+++ src/Adapter/library/Main.java
@@ -0,0 +1,18 @@
+package Adapter.library;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        LibraryAPI api = new LibraryAPIImpl();
+
+        User user = new User("Paweł","Cwik","32131212");
+
+        BookConnector connector = new BookConnector(user,api);
+
+        connector.checkAviability("Harry Potter i Zakon Feniksa");
+
+    }
+
+}

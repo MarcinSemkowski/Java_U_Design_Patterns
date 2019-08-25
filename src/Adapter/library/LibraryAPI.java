04f575e4856ae40295bcb407010636ff5a3df71c
Marcin <marcin96semkowski@gmail.com>
2019-08-25 17:41:13 +0200

    Adapter Library

---
+++ src/Adapter/library/LibraryAPI.java
@@ -0,0 +1,13 @@
+package Adapter.library;
+
+import java.time.LocalDate;
+
+public interface LibraryAPI {
+
+    boolean isAviable(String bookTitle);
+
+    LocalDate dueDate(String bookTitle, String pesel);
+
+    boolean reserve(String bookTitle, String pesel);
+
+}

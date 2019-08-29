176141c9325acaa79b340035fc1aa1875788b0a1
Marcin <marcin96semkowski@gmail.com>
2019-08-29 17:11:57 +0200

    Chain of Responsibility Intro #3 Message

--- src/Chain_of_Responsibility/intro/message/Message.java
+++ src/Chain_of_Responsibility/intro/message/Message.java
@@ -1,14 +1,21 @@
 package Chain_of_Responsibility.intro.message;
 
+import Chain_of_Responsibility.intro.officer.OfficerRank;
+
 public class Message {
     private String content;
+    private int code;
+    private OfficerRank officerRank;
 
-
-    public Message(String content) {
+    public Message(String content, int code, OfficerRank officerRank) {
         this.content = content;
+        this.code = code;
+        this.officerRank = officerRank;
     }
 
     public String getContent() {
         return content;
     }
+
+
 }

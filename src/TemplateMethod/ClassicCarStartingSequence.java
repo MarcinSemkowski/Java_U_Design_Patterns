616538c5eaf74713f223e19604487f70158a3774
Marcin <marcin96semkowski@gmail.com>
2019-08-31 17:11:57 +0200

    Template Method intro #1

---
+++ src/TemplateMethod/ClassicCarStartingSequence.java
@@ -0,0 +1,30 @@
+package TemplateMethod;
+
+public class ClassicCarStartingSequence {
+
+public void startTheCar(){
+    fastenSeatbelts();
+    startTheIgnition();
+    setTheGear();
+    go();
+
+
+}
+
+    private void go() {
+        System.out.println("Wciskamy pedał gazu");
+    }
+
+    private void setTheGear() {
+        System.out.println("Wybieramy bieg");
+    }
+
+    private void startTheIgnition() {
+        System.out.println("Przekręcamy kluczyk");
+    }
+
+    private void fastenSeatbelts() {
+        System.out.println("Zapinamy pasy");
+    }
+
+}

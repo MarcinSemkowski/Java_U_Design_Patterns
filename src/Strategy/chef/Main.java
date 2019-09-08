903a08e1549512766487d4f3eb4b03ad9ba1f235
Marcin <marcin96semkowski@gmail.com>
2019-09-08 15:06:59 +0200

    Strategy intro #1

---
+++ src/Strategy/chef/Main.java
@@ -0,0 +1,9 @@
+package Strategy.chef;
+
+public class Main {
+    public static void main(String[] args) {
+        //Nowe zamówienie - jajka na twardo
+        Chef chef = new Chef("Gordon Gessler");
+        chef.cook();
+    }
+}

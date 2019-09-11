f4a3fe76fd5cabad5498e799eb9c9017d1040de6
Marcin <marcin96semkowski@gmail.com>
2019-09-11 20:01:09 +0200

    visitor transport #5

---
+++ src/Visitor/transport/Main.java
@@ -0,0 +1,31 @@
+package Visitor.transport;
+
+import Visitor.transport.Visitor.NameTransportVisitor;
+import Visitor.transport.Visitor.PriceTransportVisitor;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("dog", 30);
+        Person person = new Person("Dawid", "Nowak", true);
+        Shipment shipment = new Shipment("PL", "4325452", false);
+
+        PriceTransportVisitor priceTransportVisitor = new PriceTransportVisitor();
+
+        List<Transportable> transportableList = Arrays.asList(animal, person, shipment);
+
+        transportableList.forEach(transportable -> transportable.accept(priceTransportVisitor));
+
+        System.out.println("----------------------");
+
+        NameTransportVisitor nameTransportVisitor = new NameTransportVisitor();
+
+        transportableList.forEach(transportable -> transportable.accept(nameTransportVisitor));
+
+    }
+
+}

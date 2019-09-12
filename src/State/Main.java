f82393a750e53c4e9058d58f5d5309d4a3bf4c26
Marcin <marcin96semkowski@gmail.com>
2019-09-12 18:21:25 +0200

    State intro #1

---
+++ src/State/Main.java
@@ -0,0 +1,14 @@
+package State;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+        coffeeMachine.insertTheCoin();
+        coffeeMachine.pushTheButton();
+        coffeeMachine.takeTheCup();
+
+    }
+
+}
